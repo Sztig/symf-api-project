81a39d86eb58712f317eebd92d81d1ee45fa0260
DDEV User <nobody@example.com>
2024-04-01 15:28:17 +0000

    api progress + added user entity

--- src/Entity/DragonTreasure.php
+++ src/Entity/DragonTreasure.php
@@ -2,16 +2,25 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Doctrine\Orm\Filter\BooleanFilter;
+use ApiPlatform\Doctrine\Orm\Filter\RangeFilter;
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
 use ApiPlatform\Metadata\Put;
+use ApiPlatform\Serializer\Filter\PropertyFilter;
 use App\Repository\DragonTreasureRepository;
 use Carbon\Carbon;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Attribute\SerializedName;
+use Symfony\Component\Validator\Constraints as Assert;
+use function Symfony\Component\String\u;
 
 #[ORM\Entity(repositoryClass: DragonTreasureRepository::class)]
 #[ApiResource(
@@ -24,10 +33,22 @@ use Doctrine\ORM\Mapping as ORM;
         new Put(),
         new Patch()
     ],
+    formats: [
+        'jsonld',
+        'json',
+        'html',
+        'jsonhal',
+        'csv' => 'text/csv'
+    ],
     normalizationContext: [
         'groups' => 'treasure:read'
-    ]
+    ],
+    denormalizationContext: [
+        'groups' => 'treasure:write'
+    ],
+    paginationItemsPerPage: 10
 )]
+#[ApiFilter(PropertyFilter::class)]
 class DragonTreasure
 {
     #[ORM\Id]
@@ -36,29 +57,43 @@ class DragonTreasure
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['treasure:read', 'treasure:write'])]
+    #[ApiFilter(SearchFilter::class, strategy: 'partial')]
+    #[Assert\NotBlank]
+    #[Assert\Length(min: 2, max: 50, maxMessage: 'Describe your loot in 50 chars or less')]
     private ?string $name = null;
 
     #[ORM\Column(type: Types::TEXT)]
+    #[Groups(['treasure:read'])]
+    #[Assert\NotBlank]
     private ?string $description = null;
 
     /**
      * The estimated value of this treasure, in gold coins
      */
     #[ORM\Column]
-    private ?int $value = null;
+    #[Groups(['treasure:read', 'treasure:write'])]
+    #[ApiFilter(RangeFilter::class)]
+    #[Assert\GreaterThanOrEqual(0)]
+    private ?int $value = 0;
 
     #[ORM\Column]
-    private ?int $coolFactor = null;
+    #[Groups(['treasure:read', 'treasure:write'])]
+    #[Assert\GreaterThanOrEqual(0)]
+    #[Assert\LessThanOrEqual(10)]
+    private ?int $coolFactor = 0;
 
     #[ORM\Column]
     private ?\DateTimeImmutable $plunderedAt = null;
 
     #[ORM\Column]
-    private ?bool $isPublished = null;
+    #[ApiFilter(BooleanFilter::class)]
+    private ?bool $isPublished = false;
 
-    public function __construct()
+    public function __construct($name = null)
     {
         $this->plunderedAt = new \DateTimeImmutable();
+        $this->name = $name;
     }
 
     public function getId(): ?int
@@ -71,16 +106,15 @@ class DragonTreasure
         return $this->name;
     }
 
-    public function setName(string $name): static
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
     public function getDescription(): ?string
     {
         return $this->description;
+    }
+
+    #[Groups(['treasure:read'])]
+    public function getShortDescription(): ?string
+    {
+        return u($this->description)->truncate(40, '...');
     }
 
     public function getValue(): ?int
@@ -124,6 +158,8 @@ class DragonTreasure
         return $this;
     }
 
+    #[Groups(['treasure:write'])]
+    #[SerializedName('description')]
     public function setTextDescription(string $description): static
     {
         $this->description = nl2br($description);
@@ -131,8 +167,23 @@ class DragonTreasure
         return $this;
     }
 
+    #[Groups(['treasure:read'])]
     public function getPlunderedAtAgo(): string
     {
         return Carbon::instance($this->plunderedAt)->diffForHumans();
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function setPlunderedAt(\DateTimeImmutable $plunderedAt): self
+    {
+        $this->plunderedAt = $plunderedAt;
+
+        return $this;
     }
 }
